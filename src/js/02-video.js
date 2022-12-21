102f9e506888ad6564af4139a5c790c327dff599
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-21 15:36:47 +0200

    task 2 ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const timeKey = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+startPlayerFromLastPosition();
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ duration, percent, seconds }) {
+  //   console.log('current place: ', seconds);
+  localStorage.setItem(timeKey, seconds);
+}
+
+function startPlayerFromLastPosition() {
+  const timePlay = localStorage.getItem(timeKey);
+
+  if (timePlay) {
+    // console.log(parseFloat(timePlay));
+    player.setCurrentTime(parseFloat(timePlay));
+  }
+}
